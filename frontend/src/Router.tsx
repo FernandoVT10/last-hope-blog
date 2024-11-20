4845985ef5fe747deafd82a20e47a3848d31b74d
FernandoVT10 <vacatamayo321@gmail.com>
2024-11-20 09:21:07 -0600

    Better route parsing

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -5,123 +5,77 @@ type Route = {
   path: string;
 };
 
-type RouterProps = {
-  routes: Route[];
-};
-
-enum TokenType {
-  URL,
-  Var,
-}
-
-type Token = {
-  type: TokenType;
-  val: string;
-};
-
-function tokenizeRoutePath(path: string): Token[] {
-  const tokens: Token[] = [{
-    type: TokenType.URL,
-    val: "",
-  }];
-
-  let collectingVar = false;
-
-  for(const c of path) {
-    if(collectingVar) {
-      if(c !== "/") {
-        tokens[tokens.length - 1].val += c;
-        continue;
-      }
-
-      collectingVar = false;
-      tokens.push({
-        type: TokenType.URL,
-        val: "",
-      });
-    }
-
-    if(c === ":") {
-      collectingVar = true;
-      tokens.push({
-        type: TokenType.Var,
-        val: "",
-      });
-      continue;
-    }
-
-    tokens[tokens.length - 1].val += c;
-  }
-
-  return tokens;
-}
-
-function comparePaths(routePath: string, path: string): boolean {
+function testPaths(routePath: string, path: string): boolean {
   if(routePath === path)
     return true;
 
-  // the routePath doesn't contain a variable, and they are not the same
-  // so they cannot match
-  if(routePath.indexOf(":") === -1)
+  const splittedRoute = routePath.split("/")
+  const splittedPath = path.split("/");
+
+  if(splittedRoute.length !== splittedPath.length)
     return false;
 
-  const maxLen = Math.max(routePath.length, path.length);
+  // skip the first one because it's always empty
+  for(let i = 1; i < splittedRoute.length; i++) {
+    const routePart = splittedRoute[i];
+    const pathPart = splittedPath[i];
 
-  let varName = "", varVal = "";
-  let collectingVar = false;
-
-  for(let i = 0; i < maxLen; i++) {
-    const routeChar = routePath[i];
-    const pChar = path[i];
-
-
-    if(routeChar === ":") {
-      collectingVar = true;
+    // skip variable part
+    if(routePart.startsWith(":")) {
+      continue;
     }
 
-    if(routeChar[i] !== pChar[i])
+    if(routePart !== pathPart)
       return false;
   }
 
   return true;
 }
 
+function getParams(routePath: string, path: string): Record<string, string> {
+  const splittedRoute = routePath.split("/")
+  const splittedPath = path.split("/");
+
+  const params: Record<string, string> = {};
+
+  // skip the first one because it's always empty
+  for(let i = 1; i < splittedRoute.length; i++) {
+    const routePart = splittedRoute[i];
+    const pathPart = splittedPath[i];
+
+    if(routePart.startsWith(":")) {
+      const varName = routePart.slice(1);
+      params[varName] = pathPart;
+    }
+  }
+
+  return params;
+}
+
 function getComponent(routes: Route[], path?: string): React.JSX.Element | null {
   if(!path)
     return null;
 
-  for(const route of routes) {
-    // if(pathContainsVar(route.path)) {
-    //   const [actualPath, varName] = route.path.split(":");
-    //
-    //   if(path.startsWith(actualPath)) {
-    //     const varVal = path.replace(actualPath, "");
-    //
-    //     const props = {
-    //       [varName]: varVal,
-    //     };
-    //
-    //     return <route.Component {...props}/>;
-    //   }
-    // } else {
-    //   if(route.path === path) {
-    //     return <route.Component/>;
-    //   }
-    // }
-    if(route.path === "*")
-      continue;
+  let defaultRoute: Route | undefined;
 
-    if(comparePaths(route.path, path)) {
-      return <route.Component/>;
+  for(const route of routes) {
+    if(route.path === "*") {
+      defaultRoute = route;
+      continue;
+    }
+
+    if(testPaths(route.path, path)) {
+      const params = getParams(route.path, path);
+      return <route.Component {...params}/>;
     }
   }
 
-  const defaultRoute = routes.find(route => {
-    return route.path === "*";
-  });
-
   return defaultRoute ? <defaultRoute.Component/> : null;
 }
+
+type RouterProps = {
+  routes: Route[];
+};
 
 function Router({ routes }: RouterProps) {
   const [pathname, setPathname] = useState<string | undefined>(undefined);
