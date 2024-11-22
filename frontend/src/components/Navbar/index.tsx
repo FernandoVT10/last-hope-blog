c6b0fd76620372bc9e2692271986eea90a569e64
FernandoVT10 <vacatamayo321@gmail.com>
2024-11-22 09:09:52 -0600

    I hate responsive design with all my soul

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,20 +1,35 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { GlobalContext } from "@/contexts";
 import { parseCssModule } from "@/utils/css";
 
 import styles from "./styles.module.scss";
+import { MenuIcon } from "@/icons";
 
 const getClassName = parseCssModule(styles);
 
 function Navbar({ title }: { title?: string }) {
+  const [isActive, setIsActive] = useState(false);
+
   const globalContext = useContext(GlobalContext);
 
   return (
     <div className={getClassName("navbar-wrapper")}>
-      <nav className={getClassName("navbar")}>
+      <nav className={getClassName("navbar", { active: isActive })}>
+        <div
+          className={getClassName("bg")}
+          onClick={() => setIsActive(false)}
+        ></div>
+
         <a className={getClassName("link")} href="/">
           <h1 className={getClassName("title")}>{ title }</h1>
         </a>
+
+        <button
+          onClick={() => setIsActive(!isActive)}
+          className={getClassName("btn")}
+        >
+          <MenuIcon size={16}/>
+        </button>
 
         <ul className={getClassName("links")}>
           <li className={getClassName("link-container")}>
